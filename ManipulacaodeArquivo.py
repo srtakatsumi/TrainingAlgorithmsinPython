acd03bc5694087248a94c7bae4de9bff60e881fa
Victoria Gabriella <73000702+srtakatsumi@users.noreply.github.com>
2021-10-12 11:52:53 -0300

    Create ManipulacaodeArquivo.py

---
+++ ManipulacaodeArquivo.py
@@ -0,0 +1,15 @@
+# ex. arquivo frase.txt contem
+
+Ola Mundo
+
+# Arquivo ManipulacaodeArquivo.py
+
+with open('/dowload/frase.txt') as text 
+  for linha in tex: 
+    print(linha) # exibi o que contem dentro do arquivo frase.txt
+    
+with open('/dowload/frase.txt') as text
+  r = tex.readlines() # dessa forma é possivel transformar as informações do arquivo frase.txt em uma lista
+   
+with open('text2.tct', 'w'): # para criar um novo arquivo W de write 
+  texto.write('Ola pessoas')
