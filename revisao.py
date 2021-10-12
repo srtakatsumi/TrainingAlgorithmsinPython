27687fb83ce503c3e19d44ef8c8160abaf6fbfcc
Victoria Gabriella <73000702+srtakatsumi@users.noreply.github.com>
2021-10-12 11:59:31 -0300

    Update revisao.py

--- revisao.py
+++ revisao.py
@@ -90,5 +90,26 @@ else:
   break
                            
                            
+# 5   
+# Considerando o dicionário com o nome dos alunos e suas respectivas notas abaixo, crie uma estrutura de repetição para percorrer cada elemento do dicionário para gravar cada aluno em um novo arquivo de texto
+# - Cada aluno deve ocupar uma linha do novo arquivo de texto
+# - O formato deve ser: nome,nota (Pedro,8.0)
+# - Após a criação do arquivo de texto, faça a leitura do arquivo e mostre todos os alunos
+# alunos = {'Pedro': 8.0, 'Maria': 10.0, 'Amilton': 7.5}
+alunos = {'Pedro': 8.0, 'Maria': 10.0, 'Amilton': 7.5}
+
+with open('alunos.txt', 'w') as arquivo:
+  for aluno, nota in alunos.items():
+    arquivo.write(f'{aluno}, {nota}\n')
+    
+with open('alunos.txt', 'r') as arquivo:
+  for linha in arquivo:
+    print(linha)                           
+                           
+                           
+                           
+                           
+                           
+                           
                            
  
