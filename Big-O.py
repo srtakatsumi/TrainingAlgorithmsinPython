a7a7cae554281c7bf6f5ceedd64ade8ddefaa567
Victoria Gabriella <73000702+srtakatsumi@users.noreply.github.com>
2021-10-16 13:25:00 -0300

    Update Big-O.py

--- Big-O.py
+++ Big-O.py
@@ -33,3 +33,62 @@ def lista2():
 
 # Funções Big-O
 
+from math import log
+import numpy as no
+import matplotlib.pyplot as plt
+
+n = np.linspace(1,10,100)
+
+labels = ['Constant', 'Logarithmic', 'Linear', 'Log Linear' , 'Quadratic','Cubic','Exponential']
+big_o = [np.ones(n.shape), np.log(n),n, n*np.log(n), n**2,n**3,2**n]
+
+plt.figure(figsize=(10,8))
+plt.ylim(0,100)
+for i in range(len(big_o)):
+  plt.plot(n,big_o[i],label = labels[i])
+plt.legend()
+plt.ylabel('Runtime')
+plt.xlabel('n')
+
+
+
+# Constant - O(1)
+
+[40] lista = [1,2,3,4,5]
+
+def constant(n):
+  print(n[0])
+  
+# Linear - O(n)
+def linear(n):
+  for i in n:
+    print(i)
+    
+
+# Quadratic - O(n^2) - polynomial
+# O(1) +  O(5) + O(n) + O(n) + O(3) 
+# O(9) + O2(n) ->  O(n) 
+def quadratic(n):
+  for i in n:
+    for j in n:
+      print(i, j)
+
+# Combination
+def conbination(n):
+  # O(1)
+  print(n[0])
+  
+  # O(5)
+  for i in range(5):
+    print('test', i)
+  # O(n)
+  for i in n:
+    print(i)
+  # O(n)
+  for i in n:
+    print(i)
+  # O(3)
+  print('Python')
+  print('Python')
+  print('Python')
+
